72b07fe0187f99278cb2f17b033e3fc86f130886
denis_ionov <syntax1141@gmail.com>
2020-05-15 17:16:41 +0300

    initial commit

---
+++ j3l1/src/Fruit.java
@@ -0,0 +1,15 @@
+public class Fruit {
+    float weight;
+
+}
+
+class Apple extends Fruit {
+    static float weight = 1.0f;
+
+}
+
+
+class Orange extends Fruit{
+    static float weight = 1.5f;
+}
+
