56f92fbb4b4886669badbb1e8a289933c12c36e9
denis_ionov <syntax1141@gmail.com>
2020-05-15 17:23:57 +0300

    second commit

--- j3l1/src/Array.java
+++ j3l1/src/Array.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Array {
+    static String[] arr = new String[]{"0", "1", "2", "3", "4"};
+    static String[] arr2 = new String[arr.length];
+
+    public static void SwapArrayNums() {
+        int n1 = 0;
+        int n2 = 4;
+        System.arraycopy(arr, 0, arr2, 0, arr.length);
+        arr2[n2] = arr[n1];
+        arr2[n1] = arr[n2];
+        System.arraycopy(arr2, 0, arr, 0, arr2.length);
+        System.out.println("Swapping element[" + n1 + "] to [" + n2 + "].\n" + Arrays.toString(arr));
+    }
+
+    public static void ArrayToArrayList() {
+        ArrayList<String> arrlist = new ArrayList<String>(Arrays.asList(arr2));
+        arrlist.add("5");
+        arrlist.add("6");
+        for (String elements: arrlist){ //просто для проверки
+            System.out.printf(elements + ", ");
+        }
+    }
 }
+
--- j3l1/src/Box.java
+++ j3l1/src/Box.java
@@ -1,3 +1,56 @@
-public class Box {
+import java.util.ArrayList;
 
+public class Box {
+    static ArrayList<Float> appleBox = new ArrayList<>();
+    static ArrayList<Float> orangeBox = new ArrayList<>();
+    static float appleWeight = 0;
+    static float orangeWeight = 0;
+
+    public static void main(String[] args) {
+
+        addToBox(15, 1);
+        getWeight();
+        System.out.println("Is boxes equal in weight : " + compare());
+    }
+
+    static public void addToBox(int appleCount, int orangeCount){
+        for (int i = 0; i < appleCount; i++) {
+            appleBox.add(Apple.weight);
+        }
+        for (int i = 0; i < orangeCount; i++) {
+            orangeBox.add(Orange.weight);
+        }
+    }
+
+    public static void getWeight(){
+        for (int i = 0; i < appleBox.size(); i++) {
+            appleWeight += appleBox.get(i);
+        }
+        for (int i = 0; i < orangeBox.size(); i++) {
+            orangeWeight += orangeBox.get(i);
+        }
+        System.out.println("Apple box weight = " + appleWeight);
+        System.out.println("Orange box weight = " + orangeWeight);
+    }
+
+
+
+    public static boolean compare(){
+        if (appleWeight==orangeWeight){
+            return true;
+        } else
+            return false;
+    }
+
+    public static void addToNewBoxApples() {
+        System.out.println();
+        ArrayList<Float> newAppleBox = new ArrayList<>(appleBox);
+        appleBox.clear();
+    }
+
+    public static void addToNewBoxOranges(){
+        ArrayList<Float> newOrangeBox = new ArrayList<>(orangeBox);
+        orangeBox.clear();
+    }
 }
+
